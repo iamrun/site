3d75f64dab67b6a3d7499ca2fb1963a18c5c3d9e
JR <dsgrtts@mail.com>
2021-08-09 19:00:45 -0400

    organized structure.

--- dashboard/split-test/assets/scripts/core.js
+++ dashboard/split-test/assets/scripts/core.js
@@ -1,37 +1,37 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    if (window.XMLHttpRequest) {
-        let http,
-            path = 'http://localhost:5500/',
-            view;
+  if (window.XMLHttpRequest) {
+    let http,
+      path = 'http://localhost:5500/',
+      view;
 
-        Array.from(document.querySelectorAll('.view-link')).forEach(e => {
-            e.addEventListener('click', () => {
-                view = e.dataset.viewPath;
-                getPage(path, view);
-            });
-        });
+    Array.from(document.querySelectorAll('.view-link')).forEach(e => {
+      e.addEventListener('click', () => {
+        view = e.dataset.viewPath;
+        getPage(path, view);
+      });
+    });
 
-        function getPage(path, view) {
-            http = new XMLHttpRequest();
-            if (!http) {
-                alert('Giving up :( cannot create an XMLHTTP instance');
-                return false;
-            }
-            http.onreadystatechange = setPage;
-            http.open('GET', path + view);
-            http.send();
-        }
-
-        function setPage() {
-            if (http.readyState === XMLHttpRequest.DONE) {
-                if (http.status === 200) {
-                    document.querySelector('main').innerHTML = http.responseText;
-                } else {
-                    console.log('There was a problem with the request.');
-                }
-            }
-        }
+    function getPage(path, view) {
+      http = new XMLHttpRequest();
+      if (!http) {
+        alert('Giving up :( cannot create an XMLHTTP instance');
+        return false;
+      }
+      http.onreadystatechange = setPage;
+      http.open('GET', path + view);
+      http.send();
     }
+
+    function setPage() {
+      if (http.readyState === XMLHttpRequest.DONE) {
+        if (http.status === 200) {
+          document.querySelector('main').innerHTML = http.responseText;
+        } else {
+          console.log('There was a problem with the request.');
+        }
+      }
+    }
+  }
 }());
